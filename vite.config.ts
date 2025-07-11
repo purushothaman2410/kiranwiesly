41687b1cd13dc3459dc33143bde0e71f50b2a46c
purushothaman Rajasekar <purushothaman.rajasekar@gmail.com>
2025-07-11 20:58:49 +0530

    setup github actions for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+ import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  base: '/<Kiranwiesly>/',  // 🔁 Replace with your actual GitHub repo name
+  // other config
+})
